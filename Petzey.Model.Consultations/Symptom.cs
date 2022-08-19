03cacc155cc283f3ae4e548c9cae4a0cc9e034c9
Sarath <88580727+sarathmahankali@users.noreply.github.com>
2022-08-19 20:19:36 +0530

    Appointment Release

---
+++ Petzey.Model.Consultations/Symptom.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Petzey.Model.Consultations
+{
+    public class Symptom
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+ 
+        public int AppointmentId { get; set; }
+    }
+}
---
+++ Petzey.Model.Consultations/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Petzey.Model.Consultations
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+   
+        public int AppointmentId { get; set; }
+    }
+}
